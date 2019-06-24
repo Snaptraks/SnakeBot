09b9e9a80287e4e4e8034697f080f3e1b4ba081f
François Hardy <snaptraks@gmail.com>
2019-06-24 15:46:06 -0400

    Add loading of cog, in case reloading fails

--- cogs/Admin.py
+++ cogs/Admin.py
@@ -15,6 +15,18 @@ class Admin(commands.Cog):
 
     @commands.command()
     @commands.is_owner()
+    async def load(self, ctx, module):
+        """Loads a cog/module."""
+        try:
+            self.bot.load_extension(module)
+        except Exception as e:
+            await ctx.message.add_reaction('\N{CROSS MARK}')
+            raise
+        else:
+            await ctx.message.add_reaction('\N{WHITE HEAVY CHECK MARK}')
+
+    @commands.command()
+    @commands.is_owner()
     async def reload(self, ctx, module):
         """Reloads a cog/module."""
         try:
