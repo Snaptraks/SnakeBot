46349a97ecdc7c1c82818eefe1b3d19eb1aac585
François Hardy <snaptraks@gmail.com>
2019-06-24 17:01:35 -0400

    Use emojis that discord understands

--- cogs/Snake.py
+++ cogs/Snake.py
@@ -5,14 +5,14 @@ import discord
 from discord.ext import commands
 
 
-class Emoji():
-    APPLE = '🍎'
-    BLACK = '⬛'
-    DOWN = '⬇️'
-    LEFT = '⬅️'
-    RIGHT = '➡️'
-    SNAKE = '🐍'
-    UP = '⬆️'
+class Emoji(Enum):
+    APPLE = '\U0001F34E'  # :apple:
+    BLACK = '\U00002B1B'  # :black_large_square:
+    DOWN = '\U00002B07'  # :arrow_down:
+    LEFT = '\U00002B05'  # :arrow_left:
+    RIGHT = '\U000027A1'  # :arrow_right:
+    SNAKE = '\U0001F40D'  # :snake:
+    UP = '\U00002B06'  # :arrow_up:
 class Snake(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
