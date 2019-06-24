7799cff2ae31785f8ab0037dfd7c8730494a11a5
François Hardy <snaptraks@gmail.com>
2019-06-24 13:57:54 -0400

    Admin cog, for stopping the bot and loading cogs

---
+++ cogs/Admin.py
@@ -0,0 +1,31 @@
+import asyncio
+import discord
+from discord.ext import commands
+
+
+class Admin(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    @commands.is_owner()
+    async def stop(self, ctx):
+        """Stops the bot quickly."""
+        self.bot.logout()
+
+    @commands.command()
+    @commands.is_owner()
+    async def reload(self, ctx, module):
+        """Reloads a cog/module."""
+        try:
+            self.bot.unload_extension(module)
+            self.bot.load_extension(module)
+        except Exception as e:
+            await ctx.message.add_reaction('\N{CROSS MARK}')
+            raise
+        else:
+            await ctx.message.add_reaction('\N{WHITE HEAVY CHECK MARK}')
+
+
+def setup(bot):
+    bot.add_cog(Admin(bot))
