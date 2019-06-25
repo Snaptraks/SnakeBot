f851c1bdee31e4c48eeb676fafe10a2c74820dd5
François Hardy <snaptraks@gmail.com>
2019-06-24 20:52:06 -0400

    First working Snake commit

--- cogs/Snake.py
+++ cogs/Snake.py
@@ -29,14 +29,17 @@ class Emoji(Enum):
 
 class Game():
     """Inspired by https://github.com/jodylecompte/Anaconda for mechanics."""
-    def __init__(self, size_x, size_y):
+    def __init__(self, size_x, size_y, ctx, bot):
         self.size_x, self.size_y = size_x, size_y
+        self.ctx, self.bot = ctx, bot
+
         self.x, self.y = self.size_x//2, self.size_y//2  # starting position
         # a list of x-coords and y-coords
         self.body = [[self.x], [self.y]]
         self.score = 0
         self.is_over = False
         self.apple = ()
+        self._apple_eaten = True
 
         self.screen = np.full((size_x, size_y), Emoji.BLACK.value)
         self.controls = (
@@ -47,30 +50,65 @@ class Game():
             Emoji.X.value,
         )
 
-    async def play(self, ctx, bot):
-        message = await ctx.send(self.display())  # starting screen
+    async def play(self):
+        message_game = await self.ctx.send(self.display())  # starting screen
+        self.message_game = message_game
         for arrow in self.controls:
-            await message.add_reaction(arrow)
+            await message_game.add_reaction(arrow)
+
+        def check(reaction, user):
+            return reaction.emoji in self.controls and\
+                user == self.ctx.author
+
+        # initial speed
+        dx, dy = 0, -1
 
         while not self.is_over:
-            reaction, user = await bot.wait_for('reaction_add', timeout=None,
-                check=lambda r, u: r.emoji in self.controls and u == ctx.author)
-            await message.remove_reaction(reaction, user)
+            try:
+                reaction, user = await self.bot.wait_for('reaction_add',
+                    timeout=1.5,
+                    check=check)
+            except asyncio.TimeoutError as e:
+                pass
+            else:
+                await message_game.remove_reaction(reaction, user)
 
-            if reaction.emoji == Emoji.UP.value:
-                dx, dy = 0, -1
-            elif reaction.emoji == Emoji.DOWN.value:
-                dx, dy = 0, 1
-            elif reaction.emoji == Emoji.RIGHT.value:
-                dx, dy = 1, 0
-            elif reaction.emoji == Emoji.LEFT.value:
-                dx, dy = -1, 0
-            elif reaction.emoji == Emoji.X.value:
-                break
+                if reaction.emoji == Emoji.UP.value:
+                    dx, dy = 0, -1
+                elif reaction.emoji == Emoji.DOWN.value:
+                    dx, dy = 0, 1
+                elif reaction.emoji == Emoji.RIGHT.value:
+                    dx, dy = 1, 0
+                elif reaction.emoji == Emoji.LEFT.value:
+                    dx, dy = -1, 0
+                elif reaction.emoji == Emoji.X.value:
+                    await self.game_over('cancelled')
+                    continue
 
             self.move(dx, dy)
 
-            await message.edit(content=self.display())
+            if (self.x, self.y) == self.apple:
+                print('moved over apple')
+                self.eat()
+            if self._apple_eaten:
+                self._spawn_apple()
+
+            # Detect collision with itself
+            if len(self.body[0]) > 1:
+                for bx, by in zip(*self.body):
+                    pass
+
+            # Detect collision with border
+            if (self.x < 0 or
+                self.x >= self.size_x or
+                self.y < 0 or
+                self.y >= self.size_y):
+                # game over
+                await self.game_over('hit a wall')
+                continue
+
+            # Update message with new game layout
+            await message_game.edit(content=self.display())
 
     def move(self, dx, dy):
         self.x += dx
@@ -83,13 +121,16 @@ class Game():
             self.body[0] = self.body[0][1:]
             self.body[1] = self.body[1][1:]
 
-    async def eat(self):
+    def eat(self):
         self.score += 1
+        self._apple_eaten = True
 
-    async def game_over(self):
+    async def game_over(self, reason):
         self.is_over = True
+        await self.message_game.delete()
+        print(reason)
 
-    async def _spawn_apple(self):
+    def _spawn_apple(self):
         while True:
             apple = (
                 np.random.randint(self.size_x),
@@ -97,6 +138,7 @@ class Game():
             )
             if apple not in np.asarray(self.body).T:
                 self.apple = apple
+                self._apple_eaten = False
                 return
 
     def _reset_screen(self):
@@ -104,8 +146,13 @@ class Game():
 
     def display(self):
         self._reset_screen()
-        print(self.body)
+        # set Apple location
+        if self.apple:
+            self.screen[self.apple] = Emoji.APPLE.value
+        # set Snake body
         self.screen[tuple(self.body)] = Emoji.SNAKE.value
+        # Transpose (.T) the screen array to match horizontal-x vertical-y in
+        # (x, y) order, unlike (i, j) for matrices
         return '\n'.join(''.join(line) for line in self.screen.T)
 
 
@@ -115,13 +162,17 @@ class Snake(commands.Cog):
 
     @commands.command()
     # @commands.dm_only()
-    async def play(self, ctx, size_x=11, size_y=11):
+    async def play(self, ctx, size_x=5, size_y=5):
         """Starts a game of Snake!"""
-        # MAX SIZE IN NORMAL MESSAGE: 198 emojis (11 x 18)
+        # MAX SIZE IN NORMAL MESSAGE: 198 emojis (18 x 11)
+        if size_x > 18:
+            size_x = 18
+        if size_y > 11:
+            size_y = 11
         print('Starting Snake Game')
 
-        game = Game(size_x, size_y)
-        await game.play(ctx, self.bot)
+        game = Game(size_x, size_y, ctx, self.bot)
+        await game.play()
 
         print('Snake Game Over')
 
